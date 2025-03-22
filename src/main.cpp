068c1cab0bbf2c57a3b350e30260a2edb76b9c37
Pascal Pieper <accounts@pascalpieper.de>
2025-03-22 10:36:00 +0100

    Added I2C bus scan. Perhaps I fried the sensor with 5v

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <pico/stdlib.h>
 #include <pico/util/queue.h>
+#include <hardware/i2c.h>
 // #include <pico/multicore.h>
 
 #include <stdio.h>
@@ -29,9 +30,86 @@ repeating_timer_t sample_timer;
 
 // --------------
 
+i2c_inst_t* setupTempI2c()
+{
+    // SHT20 is on I2C1 GP26/27
+    static constexpr unsigned sht20_sda = 26;
+    static constexpr unsigned sht20_scl = 27;
+
+    const auto init = i2c_init(i2c1, 100 * 1000); // "baud" rate 100kHz
+    printf("i2c_init(i2c1, 100 * 1000) -> %u\n", init);
+    gpio_set_function(sht20_sda, GPIO_FUNC_I2C);
+    gpio_set_function(sht20_scl, GPIO_FUNC_I2C);
+    gpio_pull_up(sht20_sda);
+    gpio_pull_up(sht20_scl);
+
+    return i2c1;
+}
+
+// #### Copied from example, tmp #####
+
+void busScan(i2c_inst_t* ic2_device)
+{
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        ret = i2c_read_blocking_until(ic2_device, addr, &rxdata, 1, false, make_timeout_time_us(500000));
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+}
+
+// ####                        #####
+
 int main() {
     setup_default_uart();
     stdio_init_all();
+
+    for (int i = 0; i <10; i++)
+    {
+        printf(".");
+        sleep_ms(500);
+        printf("\n");
+    }
+
+    i2c_inst_t* temp_i2c = setupTempI2c();
+
+    for (int i = 0; i < 5; i++)
+    {
+        uint8_t rxdata;
+        int ret;
+        ret = i2c_read_blocking_until(temp_i2c, 0x40, &rxdata, 1, false, make_timeout_time_us(100000));
+        printf("Maybe temp sensor @ 0x40: %u (status %d)\n", rxdata, ret);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf(".");
+        sleep_ms(500);
+        printf("\n");
+    }
+
+    busScan(temp_i2c);
+
+    while (true)
+    {
+    }
+
 
     queue_init(&sample_fifo, sizeof(OscCount), fifoSize);
 
