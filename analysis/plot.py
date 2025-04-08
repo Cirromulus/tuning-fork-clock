c5a10480fa12ccc7f29ac005ea72618eef59fab5
Pascal Pieper <accounts@pascalpieper.de>
2025-04-08 18:30:00 +0200

    Noticed that optimization is targeting period, not long term drift

--- analysis/plot.py
+++ analysis/plot.py
@@ -32,45 +32,61 @@ cols = 1
 rows = len(columns) - 1 # without period
 first_axis = None
 i = rows
-# for name, (column, coldesc) in columns.items():
-#     if name == 'period':
-#         continue
-#     print (f"{name}: {coldesc}")
-#     # Three integers (nrows, ncols, index)
-#     current_axis = plt.subplot(rows, cols, i, sharex=first_axis)
-#     if not first_axis:
-#         first_axis = current_axis
-#         plt.xlabel("Time [s]")
-#     else:
-#         plt.tick_params('x', labelbottom=False)
-#     plt.plot(sample_time_s, column, label=name)
-#     current_axis.set_ylabel(f"{coldesc.name} [{coldesc.unit}]")
-#     plt.ticklabel_format(style='plain')
+plt.figure("All available data")
+for name, (column, coldesc) in columns.items():
+    if name == 'period':
+        continue
+    print (f"{name}: {coldesc}")
+    # Three integers (nrows, ncols, index)
+    current_axis = plt.subplot(rows, cols, i, sharex=first_axis)
+    if not first_axis:
+        first_axis = current_axis
+        plt.xlabel("Time [s]")
+    else:
+        plt.tick_params('x', labelbottom=False)
+    plt.plot(sample_time_s, column, label=name)
+    current_axis.set_ylabel(f"{coldesc.name} [{coldesc.unit}]")
+    plt.ticklabel_format(style='plain')
 
-#     i -= 1
+    i -= 1
 # plt.title("All available Data")
 
 ## --------------------
 
 actual_period = columns['period'][0]
 estimate_period = columns['period_estimate'][0]
-initial_diff = actual_period[0] - estimate_period[0]
-print (f"Initial difference: {initial_diff}")
-estimate_period_norm = estimate_period - initial_diff
+constant_diff = estimate_period[0] - actual_period[0]
+estimate_period_norm = estimate_period - constant_diff
+estimate_diff_norm = estimate_period_norm - actual_period
+estimate_diff_cum = columns['period'][1].normalize(np.cumsum(estimate_diff_norm))
 
-fig, ax1 = plt.subplots()
+
+print (actual_period)
+print (estimate_period)
+print (f"Initial difference: {constant_diff}")
+print (estimate_period_norm)
+print (estimate_diff_norm)
+
+plt.figure("Precision")
+ax1 = plt.subplot(212)
 ax1.set_xlabel('Time [s]')
 ax2 = ax1.twinx()
 ax1.set_ylabel('Period per one Cycle [us]')
 ax1.plot(sample_time_s, actual_period, 'green', label="Measured Period")
 ax1.plot(sample_time_s, estimate_period_norm, 'red', label="Estimated Period")
+ax1.legend(loc="upper left")
 
 ax2.set_ylabel('Difference [us]')
-ax2.plot(sample_time_s, estimate_period_norm, 'teal', label='Difference')
-ax2.fill_between(sample_time_s, estimate_period_norm, 0,
-        color='teal', alpha=.5)
-plt.title("Estimation quality")
-ax1.legend()
+ax2.fill_between(sample_time_s, estimate_diff_norm, 0,
+        color='orange', alpha=.5, label='Difference per period')
 
-ax2.legend()
+ax2.legend(loc="upper right")
+
+ax3 = plt.subplot(211, sharex=ax1)
+ax3.plot(sample_time_s, estimate_diff_cum, 'teal', label='Cumulative Difference (Drift)')
+ax3.tick_params('x', labelbottom=False)
+ax3.set_ylabel('Difference [s]')
+ax3.legend()
+plt.title("Estimation quality")
+
 plt.show()
