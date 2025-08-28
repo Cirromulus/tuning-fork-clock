7861977aee1348462f9e76f2d5f4d1cb3ba79f25
Pascal Pieper <Pascal.Pieper@dlr.de>
2025-08-28 18:20:00 +0200

    Added smoothed drift to plot for analysis

--- analysis/plot.py
+++ analysis/plot.py
@@ -53,25 +53,34 @@ for name, (column, coldesc) in columns.items():
 
 ## --------------------
 
+a_smooth_number = 100 # probably "samples"
 actual_period = columns['period'][0]
 estimate_period = columns['period_estimate'][0]
 estimate_diff = actual_period - estimate_period
+estimate_diff_smooth = savgol_filter(estimate_diff, a_smooth_number, 2)
 estimate_diff_cum = columns['period'][1].normalize(np.cumsum(estimate_diff))
 
 plt.figure("Precision")
 ax1 = plt.subplot(212)
 ax1.set_xlabel('Time [s]')
 ax2 = ax1.twinx()
-ax1.set_ylabel('Period per one Cycle [us]')
-ax1.plot(sample_time_s, actual_period, 'green', label="Measured Period")
-ax1.plot(sample_time_s, estimate_period, 'red', label="Estimated Period")
-ax1.legend(loc="upper left")
 
-ax2.set_ylabel('Difference [us]')
-ax2.fill_between(sample_time_s, columns['period'][1].normalize(estimate_diff), 0,
+ax1.set_ylabel('Difference [us/period]')
+ax1.plot(sample_time_s, columns['period'][1].normalize(estimate_diff), #0,
         color='orange', alpha=.5, label='Estimation Error per period')
+ax1.plot(sample_time_s, columns['period'][1].normalize(estimate_diff_smooth),
+        color='orange', alpha=1, label='Estimation E.p.p. (smoothed)')
 
+# have this in the foreground, thus ax*2*
+ax2.set_ylabel('Period per one Cycle [us]')
+ax2.plot(sample_time_s, actual_period,
+            color='green', alpha=.6, label="Measured Period")
+ax2.plot(sample_time_s, estimate_period,
+            color='red', alpha=.7, label="Estimated Period")
+
+ax1.legend(loc="upper left")
 ax2.legend(loc="upper right")
+ax1.grid(visible='major', axis='y')
 
 ax3 = plt.subplot(211, sharex=ax1)
 ax3.plot(sample_time_s, estimate_diff_cum, 'teal', label='Cumulative Difference (Drift)')
